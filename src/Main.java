b9401e863e3fa406fb23664dc01484b6d6e1d2a6
dujinhao <jdu24@newmanstudents.org>
2023-11-16 22:49:06 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(5, 3));
+        System.out.println("Division: " + calc.divide(10, 2));
+    }
+}
